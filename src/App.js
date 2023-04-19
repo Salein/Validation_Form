e59341cf733500819d9a5df1be45de9f0951cd23
Sallein <saleinkyl@gmail.com>
2023-04-20 02:42:07 +0300

    добавил поля и состояния к ним

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
-function App() {
+export const App = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordDirty, setPasswordDirty] = useState(false)
+  const [emailDirty, setEmailDirty] = useState(false)
+  const [emailError, setEmailError] = useState("Емейл не может быть пустым")
+  const [passwordError, setPasswordError] = useState(
+    "Пароль не может быть пустым",
+  )
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "email":
+        setEmailDirty(true)
+        break
+      case "password":
+        setPasswordDirty(true)
+        break
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <h1>Регистрация</h1>
+        {emailDirty && emailError && (<p style={{ color: "red" }}>{emailError}</p>)}
+        <input onBlur={e => blurHandler(e)} name="email" type="text" placeholder="Enter your email..." />
+
+        {passwordDirty && passwordError && (<p style={{ color: "red" }}>{passwordError}</p>)}
+        <input onBlur={e => blurHandler(e)} name="password" type="password" placeholder="Enter your password..." />
+
+        <button type="submit">Registration</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
